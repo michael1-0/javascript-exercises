6bd8f0f4c66cb77aaafdcf5f908b314f2dacfab6
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2024-08-06 19:56:17 +0700

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) { 
+    const uniqueArr = [...new Set(array)];
+    for (var i = 1; i < arguments.length; i++) {
+        if (uniqueArr.includes(arguments[i])) {
+            let index = uniqueArr.indexOf(arguments[i]);
+            uniqueArr.splice(index, 1);
+        }
+        
+    }
+    return uniqueArr;
 };
 
 // Do not edit below this line
