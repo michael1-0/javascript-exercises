fad1632b9071e55bce64109b8c90e2df5a258e86
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2024-08-06 17:17:13 +0700

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let stringTemp = "";
 
+    for (let i = text.length - 1; i >= 0; i--) {
+        stringTemp += text.charAt(i);
+    }
+    
+    return stringTemp;
 };
 
 // Do not edit below this line
