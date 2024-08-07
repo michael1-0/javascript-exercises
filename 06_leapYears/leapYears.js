3fe49fe7ed4141508c634a2fabe436fc7f37dfd9
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2024-08-07 15:09:19 +0700

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ((year % 100 === 0) === true) {
+        if ((year % 400 === 0) === true) {
+            return true
+        } else {
+            return false;
+        }
+    }
+    return (year % 4 === 0) === true;
 };
 
 // Do not edit below this line
