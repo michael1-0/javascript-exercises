2a66369d753f38d86998c20362d56b42fca362d6
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2024-08-07 13:42:45 +0700

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    try {
+        if (
+            start < 0 || end < 0 ||
+            Number.isInteger(start) === false || Number.isInteger(end) === false
+        ) {
+            return "ERROR";
+        } else if (start > end) {
+            for (let i = start; i >= end; i--) {
+                sum += i;
+            }
+        } else {
+            for (let i = start; i <= end; i++) {
+                sum += i;
+            }
+        }
+    } catch(e) {
+        return 'ERROR';
+    }
+
+    return sum ;
 };
 
 // Do not edit below this line
