658db7dd447bfc9fb6e393c8f0b9008cac8867d1
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2024-08-18 11:32:35 +0700

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, element) => {
+    return sum += element;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, element) => {
+    return product *= element;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1;
+  }
+
+	let factorial = 1;
+
+  for (let i = number; i > 0; i--) {
+    factorial *= i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
