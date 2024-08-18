535572fa0f8413ed092fd7a3e5a677d5fc58e48c
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2024-08-18 12:37:01 +0700

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+    const palindromes = function (text) {
+        let modifiedText = text.replace(/[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~\s]/g, "").toLowerCase();
 
-};
+        let reverseText = modifiedText.split('').reverse().join("");
+
+        return (modifiedText === reverseText) ? true : false; 
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
