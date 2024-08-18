baed6512564f58f40c6cc948853efe1904bdff84
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2024-08-18 15:05:35 +0700

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num === 0 || num === "0") return 0;
+    let numbers = [0, 1];
+    for (let i = 2; i <= Number(num); i++) {
+        numbers[i] = numbers[i-1] + numbers[i-2];
+    } 
+    return numbers[numbers.length-1];
 };
 
 // Do not edit below this line
