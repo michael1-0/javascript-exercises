93fa74cb87921e4616a08998824c9309d8d76613
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2024-08-19 13:02:08 +0700

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.sort((a, b) => {
+        if (!(Object.hasOwn(a, "yearOfDeath"))) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (!(Object.hasOwn(b, "yearOfDeath"))) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+    })[arr.length - 1];
 };
 
 // Do not edit below this line
